45b6bc13b0dcc60fe832d7331505168a9f5d3695
kevindm181998 <kevindm181998@gmail.com>
2021-10-11 15:46:16 -0500

    dasd

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,5 @@ from . import views
 
 
 urlpatterns = [
-    #path('', views.index, name='index'),
     path('',views.login_view, name="login"),
 ]
--- app/views.py
+++ app/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template, Context
 
-   
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render
-
-def index(request):
-    return HttpResponse("Eh, eh epa colombia...")
-
-
 def login_view(request):
 
     doc_externo=open('app/templates/base.html')
