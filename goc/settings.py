d0480421b79517672f87e6414e0582a0e60ae023
kevindm181998 <kevindm181998@gmail.com>
2021-10-13 14:06:37 -0500

    dasdsad

--- goc/settings.py
+++ goc/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
-    'django_bootstrap_icons'
+    'django_bootstrap_icons',
+    'goc'
 ]
 
 MIDDLEWARE = [
@@ -143,6 +144,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Added manually 
+STATICFILES_DIRS = [
+    BASE_DIR / "app/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
