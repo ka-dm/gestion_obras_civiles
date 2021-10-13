178096461337e4f97451340deff0389c0bd16393
kevindm181998 <kevindm181998@gmail.com>
2021-10-12 22:22:25 -0500

    dasd

--- app/views.py
+++ app/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template, Context
+from django.template.loader import get_template
+from django.shortcuts import render
+
 
 def login_view(request):
 
-    doc_externo=open('app/templates/base.html')
-    plt=Template(doc_externo.read()) 
-    doc_externo.close()
-
-    ctx=Context()
-
-    docuemento=plt.render(ctx)
+    doc_externo=get_template('base.html')
+    docuemento=doc_externo.render({})
 
     return HttpResponse(docuemento)
 
